3bcad094d2a61846a9b9244acd206d612ed295ec
lobtao <lobtao@163.com>
2017-05-24 19:28:01 +0800

    add helper functions

---
+++ src/Helper.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017-5-24
+ * Time: 19:24
+ */
+
+
+if (!function_exists('ROOT')) {
+    /**
+     * 当前应用URL路径
+     * @return string
+     */
+    function ROOT() {
+        // 基础替换字符串
+        $request = \think\Request::instance();
+        $base = $request->root();
+        $root = strpos($base, '.') ? ltrim(dirname($base), DS) : $base;
+        if ('' != $root) {
+            $root = '/' . ltrim($root, '/');
+        }
+        return $root;
+    }
+}
+
+
+if (!function_exists('V')) {
+    /**
+     * 快捷校验方法
+     * @param \think\Validate $validate
+     * @param String $scenario
+     * @param Array $params
+     * @param bool|true $showException
+     * @return string
+     * @throws Exception
+     */
+    function V($validate, $scenario, $params, $showException = true) {
+        //校验输入值
+        $msg = '';
+        if (!$validate->scene($scenario)->check($params)) {
+            $msg = $validate->getError();
+
+            if ($showException) throw new \Exception($msg);
+        }
+        return $msg;
+    }
+}
