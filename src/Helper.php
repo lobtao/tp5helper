c2f64750c03a1288b42de14f9ec55726607042f1
lobtao <lobtao@qq.com>
2017-10-25 15:00:08 +0800

    add layout
    add layout

--- src/Helper.php
+++ src/Helper.php
@@ -46,3 +46,62 @@ if (!function_exists('V')) {
         return $msg;
     }
 }
+
+if (!function_exists('getValue')) {
+    /**
+     * 获取表单formData里字段值
+     * @param $array
+     * @param $key
+     * @param int $type
+     * @return int|string
+     */
+    function getValue($array, $key, $type = 0) {
+        switch ($type) {
+            case 0://字符串
+                return array_key_exists($key, $array) ? $array[$key] : '';
+                break;
+            case 1://整数、浮点数
+                return array_key_exists($key, $array) ? $array[$key] : 0;
+                break;
+        }
+    }
+}
+
+if (!function_exists('createUrl')) {
+    /**
+     * 生成url访问地址
+     * @param $router
+     * @return string
+     */
+    function createUrl($router) {
+        if (strpos($_SERVER['HTTP_USER_AGENT'], 'yssoft'))//需要在apicloud config.xml里配置<preference name="userAgent" value="yssoft" />
+            return sprintf("func_openWin('%s','%s')", url($router, '', false, true), config('title'));
+        else
+            return url($router, '', false, true);
+
+    }
+}
+
+if (!function_exists('layout')) {
+    /**
+     * 布局母板页输出
+     * @param string $template
+     * @param array $vars
+     * @param array $replace
+     * @param int $code
+     * @return $this|\think\Response|\think\response\Json|\think\response\Jsonp|\think\response\Redirect|\think\response\View|\think\response\Xml
+     */
+    function layout($template = '', $vars = [], $replace = [], $code = 200) {
+        //渲染子页面
+        $response = \think\Response::create($template, 'view', $code);
+        $response->replace($replace)->assign($vars);
+        //渲染母板页
+        if (config('template.layout_on')) {
+            return \think\Response::create('./' . config('template.layout_name'), 'view', $code)->replace([
+                config('template.layout_item') => $response->getContent()
+            ]);
+        } else {
+            return $response;
+        }
+    }
+}
